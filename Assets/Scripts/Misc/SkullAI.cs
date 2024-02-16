9ed6521b656d7c71a43483d18e872756bfb6d626
M0ooncake <makenzieithomson@gmail.com>
2024-02-16 13:54:44 -0400

    improved enemy AI
    added paththing to the enemy so it could walk back and fourth. that took a lot longer then i hoped

--- Assets/Scripts/Misc/SkullAI.cs
+++ Assets/Scripts/Misc/SkullAI.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class SkullAI : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    SpriteRenderer sr;
+    public int HealthPoints = 2;
+    public float speed = 2;
+    public GameObject PointA;
+    public GameObject PointB;
+    public Animator anim;
+    public Transform currentPoint;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        currentPoint = PointB.transform;
         
     }
 
@@ -14,5 +27,40 @@ public class SkullAI : MonoBehaviour
     void Update()
     {
         
+
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == PointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2 (-speed, 0);
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            flip();
+            currentPoint = PointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            flip();
+            currentPoint = PointB.transform;
+        }
+        
     }
+    
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position); 
+    }
+
 }
